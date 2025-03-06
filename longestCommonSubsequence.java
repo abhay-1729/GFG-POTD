08c0e3fb8cdb6a12e70800817710cb2af58bd70a
abhay-1729 <136903155+abhay-1729@users.noreply.github.com>
2025-03-06 08:59:36 +0530

    Create longestCommonSubsequence.java

---
+++ longestCommonSubsequence.java
@@ -0,0 +1,23 @@
+//Problem: Longest Common Subsequence
+// GFG
+
+class Solution {
+    static int lcs(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        int prev[] = new int[m + 1];
+        int cur[] = new int[m + 1];
+        for (int ind1 = 1; ind1 <= n; ind1++) {
+            for (int ind2 = 1; ind2 <= m; ind2++) {
+                if (s1.charAt(ind1 - 1)
+                    == s2.charAt(ind2 - 1))
+                    cur[ind2] = 1 + prev[ind2 - 1];
+                else
+                    cur[ind2] = Math.max(prev[ind2],
+                                         cur[ind2 - 1]);
+            }
+            prev = (int[])(cur.clone());
+        }
+        return prev[m];
+    }
+}
