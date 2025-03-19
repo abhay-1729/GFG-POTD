9b3a8c9f27b8e4f375cab59c84f34ae643dc6e5b
abhay-1729 <136903155+abhay-1729@users.noreply.github.com>
2025-03-19 19:04:59 +0530

    Create 18-03-2025_Partition_Equal_Subset_Sum.java

---
+++ 18-03-2025_Partition_Equal_Subset_Sum.java
@@ -0,0 +1,35 @@
+//Problem: Partition Equal Subset Sum (GFG)
+class Solution {
+    static boolean equalPartition(int arr[]) {
+        // code here
+        int totSum = 0;
+        int n=arr.length;
+        for (int i = 0; i < n; i++) {
+            totSum += arr[i];
+        }
+        if (totSum % 2 == 1)
+            return false;
+        else {
+            int k = totSum / 2;
+            boolean prev[] = new boolean[k + 1];
+            prev[0] = true;
+            if (arr[0] <= k) {
+                prev[arr[0]] = true;
+            }
+            for (int ind = 1; ind < n; ind++) {
+                boolean cur[] = new boolean[k + 1];
+                cur[0] = true;
+                for (int target = 1; target <= k; target++) {
+                    boolean notTaken = prev[target];
+                    boolean taken = false;
+                    if (arr[ind] <= target) {
+                        taken = prev[target - arr[ind]];
+                    }
+                    cur[target] = notTaken || taken;
+                }
+                prev = cur;
+            }
+            return prev[k];
+        }
+    }
+}
