5d677f93a7215c49db2eec66ffca07d51e67a75c
abhay-1729 <136903155+abhay-1729@users.noreply.github.com>
2025-04-16 22:34:33 +0530

    Create 16-04-2025_Floyd_Warshall.java

---
+++ 16-04-2025_Floyd_Warshall.java
@@ -0,0 +1,18 @@
+//Problem: Floyd Warshall(GFG)
+class Solution {
+    public void floydWarshall(int[][] dist) {
+        // Code here
+        int n = dist.length;
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    // Prevent overflow by ensuring both paths exist
+                    if (dist[i][k] < 100000000 && dist[k][j] < 100000000) {
+                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                    }
+                }
+            }
+        }
+    }
+}
